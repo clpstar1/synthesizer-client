cdd34c7a402852812c1e490ae178574ebd43fcfc
Steffen <steffen.lesch96@gmail.com>
2021-09-04 14:34:29 +0200

    delete client stuff from repo

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import PlayButton from './ui/play-button';
+
+function UserInterface() {
+    return (
+        <div>
+          <PlayButton />
+        </div>
+    )
+}
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(<UserInterface />, document.body);
 }
 
 render();
